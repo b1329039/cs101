2fd0f331c02b5a54314eb578982cfd900f54f05b
b1329039 <ac9132722456869@gmail.com>
2024-12-05 15:07:42 +0800

    Create main1.c

---
+++ EX3/main1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("2");
+    }
+    else{
+        printf("gg");
+    }
+
+    return 0;
+}
