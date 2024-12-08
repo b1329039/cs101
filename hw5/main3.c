eebebd612c9c6051864a283bdd8a428d4a65c4f9
b1329039 <ac9132722456869@gmail.com>
2024-12-09 02:18:19 +0800

    Create main3.c

---
+++ hw5/main3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    for(num=1;num<=9;num++){
+        printf("%d*1=%d\n%d*2=%d\n%d*3=%d\n%d*4=%d\n%d*5=%d\n%d*6=%d\n%d*7=%d\n%d*8=%d\n%d*9=%d\n\n",num,num*1,num,num*2,num,num*3,num,num*4,num,num*5,num,num*6,num,num*7,num,num*8,num,num*9);
+    }
+    return 0;
+}
